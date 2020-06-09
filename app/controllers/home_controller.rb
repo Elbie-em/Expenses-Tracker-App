a725bedc280fa44ad4d17c243466f3391b7ba805
elbie-em <emoonga1993@gmail.com>
2020-06-09 22:52:46 +0200

    Alerts dismissable,main.html modification

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:main]
+
   def index
+    redirect_to home_main_path if current_user
   end
 
   def main
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :home, only: [:index,:main]
+  resources :home, only: [:index]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/main'
+
   root "home#index"
 end
