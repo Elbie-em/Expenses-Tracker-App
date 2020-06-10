1b09f83399c6cf11664febf70322d56db9174842
elbie-em <emoonga1993@gmail.com>
2020-06-10 23:40:56 +0200

    styling expense views, modifying expense controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,6 +4,7 @@ class ExpensesController < ApplicationController
   # GET /expenses
   # GET /expenses.json
   def index
+    #@user = User.find_by(id: current_user.id)
     @expenses = Expense.all
   end
 
@@ -25,10 +26,11 @@ class ExpensesController < ApplicationController
   # POST /expenses.json
   def create
     @expense = Expense.new(expense_params)
+    @expense.user = current_user
 
     respond_to do |format|
       if @expense.save
-        format.html { redirect_to @expense, notice: 'Expense was successfully created.' }
+        format.html { redirect_to expenses_path, notice: 'Expense was successfully created.' }
         format.json { render :show, status: :created, location: @expense }
       else
         format.html { render :new }
@@ -69,6 +71,6 @@ class ExpensesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def expense_params
-      params.require(:expense).permit(:name, :amount, :user_id, :group_id)
+      params.require(:expense).permit(:name, :amount, :group_id)
     end
 end
