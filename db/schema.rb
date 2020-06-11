86e700c2a8bb29a19d20e5881570a7bab2b4c52c
elbie-em <emoonga1993@gmail.com>
2020-06-11 16:39:27 +0200

    editing user params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_204155) do
+ActiveRecord::Schema.define(version: 2020_06_11_135843) do
 
   create_table "expenses", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_204155) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
