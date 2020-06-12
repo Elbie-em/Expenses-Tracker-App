14a7600c344746d0614cc76d071ccb263091d25a
elbie-em <emoonga1993@gmail.com>
2020-06-12 14:45:37 +0200

    testing user model: validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { maximum: 20 }
+
   has_many :groups
   has_many :expenses
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,3 +65,11 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# Configure shouldamatchers
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
